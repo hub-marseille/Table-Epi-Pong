7afb02bb4f1b9d8a4b47a4f163248ec94c3e1898
guigur <guigur13@gmail.com>
2017-06-26 20:17:11 +0200

    added demo code

--- Demo1/Demo1.ino
+++ Demo1/Demo1.ino
@@ -1,22 +1,18 @@
-//simple A4988 connection
-//jumper reset and sleep together
-//connect  VDD to Arduino 3.3v or 5v
-//connect  GND to Arduino GND (GND near VDD)
-//connect  1A and 1B to stepper coil 1
-//connect 2A and 2B to stepper coil 2
-//connect VMOT to power source (9v battery + term)
-//connect GRD to power source (9v battery - term)
+/*
+ * By guigur 2017
+ * EpiPong Project
+ */
 
+int       stepPin = A0;
+int       dirPin = A1;
+int       ebl = 38;
+int       a = 0;
 
-int stepPin = A0;  //connect pin 13 to step
-int dirPin = A1;  // connect pin 12 to dir
-int ebl = 38;
-int a = 0;     //  gen counter
-#include <Encoder.h>
+#include  <Encoder.h>
 long oldPosition  = 0;
 Encoder myEnc(3, 2);
 
-void setup() 
+void      setup() 
 {                
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
@@ -25,16 +21,15 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop() {
+void      loop() {
   digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular directi
-  long newPosition = myEnc.read()/2;
+  long newPosition = myEnc.read()/4;
 
     if (newPosition > oldPosition)
     {
       digitalWrite(dirPin,HIGH);
       for(int x = 0; x < (newPosition - oldPosition); x++)
       {
-        //Changes the rotations direction
         digitalWrite(stepPin,HIGH); 
         delayMicroseconds(500); 
         digitalWrite(stepPin,LOW); 
@@ -51,7 +46,7 @@ void loop() {
         delayMicroseconds(500);
         digitalWrite(stepPin,LOW);
         delayMicroseconds(500);
-        digitalWrite(dirPin,LOW); //Changes the rotations direction
+        digitalWrite(dirPin,LOW);
       }
     oldPosition = newPosition;
     Serial.println(newPosition);
