c9136097c6066c81f7b518b0dcf38d25fe9f7ac3
guigur <guigur13@gmail.com>
2017-09-14 09:56:48 +0200

    added all the files in the laptop

---
+++ PROG/pong_tests/pong_tests/pong_tests.cpp
@@ -0,0 +1,13 @@
+// pong_tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "test" << std::endl;
+	while (1);
+	return 0;
+}
+
